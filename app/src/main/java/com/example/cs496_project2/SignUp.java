c3d125cb96e885b28dc50b9b875941b7af6779f5
sally <https://github.com/youngjae99/CS496_MadCamp>
2020-07-19 17:36:31 +0900

    feat: add signup retrofit2, but intent double click problem

--- app/src/main/java/com/example/cs496_project2/SignUp.java
+++ app/src/main/java/com/example/cs496_project2/SignUp.java
@@ -1,10 +1,13 @@
 package com.example.cs496_project2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,7 +20,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SignUp extends AppCompatActivity {
+
+    // Variables for server communication
+    private RetrofitInterface retrofitInterface;
+    private static String TAG = "SignUpActivity";
+    String register_result;
 
     String signupID="", signupPW="", signupNUM="";
     EditText idET,pwET,numET;
@@ -25,6 +37,7 @@ public class SignUp extends AppCompatActivity {
     ArrayList<String> data;
     ArrayAdapter<String> arrayAdapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +80,28 @@ public class SignUp extends AppCompatActivity {
                 signupNUM=numET.getText().toString();
                 //사진 uri(?)도 디비에 올려야함
                 // 디비에 사용자 정보 올리기
+
+                // RetroFit
+                retrofitInterface = RetrofitUtility.getRetrofitInterface();
+                retrofitInterface.user_register(new RegisterReq(signupID, signupPW, signupNUM)).enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if (response.isSuccessful()) {
+                            register_result = response.body();
+                            Log.d(TAG, register_result);
+                            Intent signupIntent = new Intent(SignUp.this, LoginActivity.class);
+                            startActivity(signupIntent);
+                            Toast.makeText(SignUp.this,"Sign-up Success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            int statusCode  = response.code();
+                            Log.d(TAG, String.valueOf(statusCode));
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Log.e(TAG, t.toString());
+                    }
+                });
             }
         });
 
